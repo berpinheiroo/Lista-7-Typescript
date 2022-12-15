f96d9d225db06c9e509ad5dacfe52909182e4fd9
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-12-15 20:46:53 -0300

    Ex 2

--- 7.js
+++ 7.js
@@ -1,18 +1,37 @@
-console.log(RepeatedLettersCounter("aabcde333"));
-function RepeatedLettersCounter(word) {
-    var letterCounter = 0;
-    var arrayWord = word.split('');
-    arrayWord.forEach(function (x) {
-        var repeatedCounter = 0;
-        for (var i = 0; i < arrayWord.length; i++) {
-            if (x == arrayWord[i]) {
-                repeatedCounter++;
-            }
-        }
-        if (repeatedCounter > 1) {
-            arrayWord = arrayWord.filter(function (letter) { return letter == x; });
-            letterCounter++;
-        }
-    });
-    return letterCounter;
+// console.log(RepeatedLettersCounter("aabcde333"))
+// function RepeatedLettersCounter(word : string) : number{
+//     let letterCounter : number = 0
+//     let arrayWord : string [] = word.split('')
+//     arrayWord.forEach((x) => {
+//         let repeatedCounter : number = 0
+//         for(let i : number = 0; i < arrayWord.length; i++){
+//             if(x == arrayWord[i]){
+//                 repeatedCounter++
+//             }
+//         }
+//         if(repeatedCounter > 1){
+//             arrayWord = arrayWord.filter(letter => letter == x)
+//             letterCounter++
+//         }
+//     })
+//     return letterCounter
+// }  
+var names = ["Bernardo", "Argollo"];
+ReturnText(names);
+function ReturnText(names) {
+    if (names.length == 0) {
+        console.log("No one liked it.");
+    }
+    else if (names.length == 1) {
+        console.log(names[0] + " liked it.");
+    }
+    else if (names.length == 2) {
+        console.log(names[0] + " e " + names[1] + " liked it.");
+    }
+    else if (names.length == 3) {
+        console.log(names[0] + ", " + names[1] + " and " + names[2] + " liked it.");
+    }
+    else if (names.length > 3) {
+        console.log(names[0] + ", " + names[1] + " and others " + (names.length - 2) + " liked it.");
+    }
 }
--- 7.ts
+++ 7.ts
@@ -1,20 +1,39 @@
-console.log(RepeatedLettersCounter("aabcde333"))
+// console.log(RepeatedLettersCounter("aabcde333"))
 
-function RepeatedLettersCounter(word : string) : number{
-    let letterCounter : number = 0
-    let arrayWord : string [] = word.split('')
-    arrayWord.forEach((x) => {
-        let repeatedCounter : number = 0
-        for(let i : number = 0; i < arrayWord.length; i++){
-            if(x == arrayWord[i]){
-                repeatedCounter++
-            }
-        }
-        if(repeatedCounter > 1){
-            arrayWord = arrayWord.filter(letter => letter == x)
-            letterCounter++
-        }
-    })
-    return letterCounter
-}  
+// function RepeatedLettersCounter(word : string) : number{
+//     let letterCounter : number = 0
+//     let arrayWord : string [] = word.split('')
+//     arrayWord.forEach((x) => {
+//         let repeatedCounter : number = 0
+//         for(let i : number = 0; i < arrayWord.length; i++){
+//             if(x == arrayWord[i]){
+//                 repeatedCounter++
+//             }
+//         }
+//         if(repeatedCounter > 1){
+//             arrayWord = arrayWord.filter(letter => letter == x)
+//             letterCounter++
+//         }
+//     })
+//     return letterCounter
+// }  
 
+
+
+let names : string[] = [Bernardo, Argollo]
+
+ReturnText(names)
+
+function ReturnText(names : string[]) : void{
+    if (names.length == 0){
+        console.log("No one liked it.")
+    }else if(names.length == 1){
+        console.log(names[0]+ " liked it.")
+    }else if(names.length == 2){
+        console.log(names[0]+ " e " +names[1]+ " liked it.")
+    }else if(names.length == 3){
+        console.log(names[0]+ ", " +names[1]+ " and " +names[2]+ " liked it.")
+    }else if(names.length > 3){
+        console.log(names[0]+ ", " +names[1]+ " and others " +(names.length - 2)+ " liked it.")
+    }
+}
